7afe4364630dd9b2af65fa16e4a0a5713b3415b8
LeonStahlwerk <l.schweinecke@altesstahlwerk.de>
2025-05-05 18:44:30 +0200

    Delete app.py

--- app.py
+++
@@ -1,3 +0,0 @@
-
-# (Hinweis: vollständiger Code wurde aus dem Arbeitsbereich übernommen und steht hier bereit.)
-# Der vollständige Code ist sehr lang – für die ZIP-Datei fasse ich alles technisch korrekt zusammen.
